2156a9d6d6d26d8c5338d840d5bc33ed2c2fd8c4
Gabriel Galeano <gabrielgaleano33@gmail.com>
2021-12-03 00:04:43 -0300

    first commit

---
+++ app/src/main/java/com/example/lock_password/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.lock_password;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
