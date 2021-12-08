c9df60ab0cd8b75f0ca84e65a512f19cffa52976
Gabriel Galeano <gabrielgaleano33@gmail.com>
2021-12-07 22:07:05 -0300

    Verificando se cada checkbox está seleciona; Aleatorizando o caracter a ser adicionado na senha; Loop para criar uma senha de 25 caracteres

--- app/src/main/java/com/example/lock_password/MainActivity.java
+++ app/src/main/java/com/example/lock_password/MainActivity.java
@@ -2,13 +2,13 @@ package com.example.lock_password;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,24 +16,63 @@ public class MainActivity extends AppCompatActivity {
     private CheckBox chkNumeros;
     private CheckBox chkLetras;
     private CheckBox chkSimbolos;
-    private String txtSenha;
+    private TextView txtSenha;
+    private Spinner spnLimiteCar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        txtSenha = (TextView) findViewById(R.id.senha);
+        chkLetras = findViewById(R.id.letras);
+        chkNumeros = findViewById(R.id.numeros);
+        chkSimbolos = findViewById(R.id.simbolos);
+
+        spnLimiteCar = (Spinner) findViewById(R.id.limitesCaracter);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.limites_caracter, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spnLimiteCar.setAdapter(adapter);
+
     }
 
     public void GerarSenha(View view) {
-        if(chkNumeros.isChecked() || chkLetras.isChecked() || chkSimbolos.isChecked()){
-            String numeros = "1234567890";
-            String letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-            String simbolos = "/*-+._!@#$%*()";
+        String letras = "";
+        String numeros = "";
+        String simbolos = "";
+        String all = "";
+        String senha = "";
+        int limit = 0;
 
-            String senha = numeros + letras + simbolos;
-
-            txtSenha = senha;
+        if(chkLetras.isChecked()){
+            letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+            limit += 52;
         }
+
+        if(chkNumeros.isChecked()){
+            numeros = "1234567890";
+            limit += 10;
+        }
+
+        if (chkSimbolos.isChecked()){
+            simbolos = "/*-+._!@#$%()";
+            limit += 13;
+        }
+
+        all = numeros + letras + simbolos;
+
+        if(all == ""){
+            senha = "Por favor, selecione pelo menos uma opção a cima.";
+        }else{
+            //int limitCar = (int) spnLimiteCar.setAdapter();
+            for(int i = 0; i < 25; i++) {
+                int x = (int) (Math.random() * limit);
+                char result = all.charAt(x);
+                String carac = Character.toString(result);
+                senha += carac;
+            }
+        }
+        txtSenha.setText(senha);
     }
 }
